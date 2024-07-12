9d966f9e1befc5ed903329fb6bb1bf033701c6a4
diogobarroscode <diogobarros.code@gmail.com>
2024-07-12 19:38:54 -0300

    feat: Criação da classe ContaTerminal. Criação da aplicação e iteração com o cliente em app.java

--- ContaBanco/src/App.java
+++ ContaBanco/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        ContaTerminal conta = new ContaTerminal();
+
+        System.out.println("Digite o número da conta: ");
+        conta.setNumero(scanner.nextInt());
+
+        System.out.println("Digite o número da agência: ");
+        conta.setAgencia(scanner.next());
+
+        System.out.println("Digite seu nome: ");
+        conta.setCliente(scanner.next());
+
+        System.out.println("Digite o saldo da sua conta: ");
+        conta.setSaldo(scanner.nextDouble());
+
+        System.out.println("Olá " + conta.getCliente() + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.getAgencia() + ", conta " + conta.getNumero() + " e seu saldo " + conta.getSaldo() + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,40 @@
+public class ContaTerminal{
+  
+  int numero;
+  String agencia;
+  String nomeCliente;
+  double saldo;
+
+  public int getNumero(){
+    return numero;
+  }
+
+  public void setNumero(int numero){
+    this.numero = numero;
+  }
+
+  public String getAgencia(){
+    return agencia;
+  }
+
+  public void setAgencia(String agencia){
+    this.agencia = agencia;
+  }
+
+  public String getCliente(){
+    return nomeCliente;
+  }
+
+  public void setCliente(String nomeCliente){
+    this.nomeCliente = nomeCliente;
+  }
+
+  public double getSaldo(){
+    return saldo;
+  }
+
+  public void setSaldo(double saldo){
+    this.saldo = saldo;
+  }
+
+}
